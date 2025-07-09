ce241e6fffab95273e109e90e348399e2e74f16f
Suryansh Tripathi <tripathisuryansh800@gmail.com>
2025-07-09 20:59:41 +0530

    ..

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app.secret_key = 'supersecretmre'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
